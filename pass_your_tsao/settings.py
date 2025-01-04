2e87edb594c1ab04b06c3c70aafa26ba92499890
xealmi <maksimfed084@gmail.ru>
2025-01-04 21:42:16 +0300

    add file config.py with local settings

--- pass_your_tsao/settings.py
+++ pass_your_tsao/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m=36z=y4g8vpkqy!4!hy+l%b7m4z7%yh4oy)h%i1iz=4%)o295'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config.DEBUG
 
 ALLOWED_HOSTS = []
 
